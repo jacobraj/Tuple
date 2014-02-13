84b145838e36a96002141b99500f3f19046d12d1
Jacobraj Benet <jbenet@jbenet-nylm.local>
2014-02-13 16:11:30 -0600

    Initial Commit

---
+++ Tuple/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Tuple
+//
+//  Created by Jacobraj Benet on 2/13/14.
+//  Copyright (c) 2014 Jacobraj Benet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
